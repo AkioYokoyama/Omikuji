7a8a2116e8c7eea6464489b85f9dcc628519e2cd
mk3250d07295y@aol.com <Yoika>
2022-12-31 13:09:18 +0900

    Add random number processing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,8 @@
-#![cfg_attr(
+extern crate rand;
+
+use rand::{thread_rng, Rng};
+
+#[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
@@ -6,7 +10,9 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn omikuji(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let mut rng = thread_rng();
+    let rand: u32 = rng.gen_range(0..8);
+    format!("Hello, {}:{}! You've been greeted from Rust!", name, rand)
 }
 
 fn main() {
